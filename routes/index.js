633afdf2053ec1b006dddf363694b4d60af70e10
gnemek20 <gnemek20@gmail.com>
2023-08-02 01:51:31 +0900

    console check

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ router.get('/', function(req, res, next) {
 // POST METHOD
 router.post('/uploadImage', upload.any(), (req, res) => {
   const { file } = req;
+  console.log('\n\n\n req \n\n\n');
+  console.log(req);
+  console.log('\n\n\n body \n\n\n');
+  console.log(req.body)
   console.log(file);
 
   res.status(200).send();
